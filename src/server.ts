e584ff38053a2fa46a1749dca6f5faf6baf28838
michael <abdulazizklichev1@gmail.com>
2024-02-23 07:28:02 +0900

    feat:Initial Setup

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+//console.log('run buldi');
+
+//Architectural pattern : MVC, Dependence Injection, MVP   =>backend ma'lumotlarni oqimini tartibga soladigan arxitektura
+// MVC =Model View Controller  => asosan backendda ishlatiladi
+// DI = Dependence Injection  aspect oriented programming => modullarni ishlatish tizimida ishlatiladi NestJS DI da quriligan 
+//Design pattern:  Middleware, Decotar   => ma'lum bir bulaklarini strukturasini yechishda qullaniladi
