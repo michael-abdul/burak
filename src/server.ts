45c806200122f74f34ae3c3171026877b86c8eba
michael <abdulazizklichev1@gamil.com>
2024-02-25 10:03:56 +0900

    feat: integeration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,3 @@
-//console.log('run buldi');
+import dotenv from 'dotenv';
+dotenv.config();
 
-//Architectural pattern : MVC, Dependence Injection, MVP   =>backend ma'lumotlarni oqimini tartibga soladigan arxitektura
-// MVC =Model View Controller  => asosan backendda ishlatiladi
-// DI = Dependence Injection  aspect oriented programming => modullarni ishlatish tizimida ishlatiladi NestJS DI da quriligan 
-//Design pattern:  Middleware, Decotar   => ma'lum bir bulaklarini strukturasini yechishda qullaniladi
