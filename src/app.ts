11f795044af0d299aec4d5d5525aca0606c725aa
michael <abdulazizklichev1@gamil.com>
2024-03-08 09:35:12 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,19 @@ import router from './router';
 import routerAdmin from "./router-admin"
 import morgan from "morgan";
 import { MORGAN_FORMAT } from './libs/config';
+
+import session from 'express-session'; 
+import ConnectMongoDB from 'connect-mongodb-session';
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+    uri:String(process.env.MONGO_URL),
+    collection: "sessions",
+});
+
+
+
+
+
 // 1-ENTRANCE
 
 const app = express();
@@ -13,7 +26,17 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 // 2-SESSIONS
-
+app.use(
+    session({
+        secret:String (process.env.SESSION_SECRET),
+        cookie:{
+            maxAge: 1000 * 3600 * 3,  //3h
+        },
+        store:store,
+        resave:true,   //10:30h =>13:30     false:12:00 ~13:30
+        saveUninitialized:true,
+    })
+    );
 // 3-VIEWS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
