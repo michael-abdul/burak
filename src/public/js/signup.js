0a5977cfedd47d5c19deec5a18663003345a3a51
michael <abdulazizklichev1@gamil.com>
2024-03-20 13:57:55 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -4,10 +4,9 @@ $(function (){
     let filename;
     fileTarget.on("change" , function (){
         if(window.FileReader) {
-            const uploadFile = $(this)[0].files[0];
-            // console.log("uploadFile",uploadFile);
-            const fileType = uploadFile["type"];
-            const validImageType = ["image/jpg","image/jpeg","image/png"];
+            const uploadFile = $(this)[0].files[0],
+             fileType = uploadFile["type"],
+             validImageType = ["image/jpg","image/jpeg","image/png"];
             if(!validImageType.includes(fileType)){
                 alert( "Please insert only jpeg, jpg and png!")
 
@@ -26,10 +25,10 @@ $(function (){
     });
   });
   function  validateSignupForm() {  
-    const memberNick = $(".member-nick").val();
-    const memberPhone= $(".member-phone").val();
-    const memberPassword = $(".member-password").val();
-    const confirmPassword = $(".confirm-password").val();
+    const memberNick = $(".member-nick").val(),
+     memberPhone= $(".member-phone").val(),
+     memberPassword = $(".member-password").val(),
+     confirmPassword = $(".confirm-password").val();
 
     if(
         memberNick ===  "" ||
